22d16940dba9786b14624105b5a546f180ee1840
Prozid <daniprolongo@hotmail.com>
2015-05-30 16:57:02 +0200

    Fixed checksum's check when the data is sended.

--- PBioSocket.cs
+++ PBioSocket.cs
@@ -119,6 +119,7 @@ namespace PBioSvc
 
         private static void ReceiveCallback(IAsyncResult ar)
         {
+            String content = String.Empty;
             try
             {
                 // Retrieve the state object and the client socket 
@@ -128,26 +129,26 @@ namespace PBioSvc
 
                 // Read data from the remote device.
                 int bytesRead = client.EndReceive(ar);
-
-
                 if (bytesRead > 0)
                 {
                     // There might be more data, so store the data received so far.
                     state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
 
-                    // Get the rest of the data.
-                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                        new AsyncCallback(ReceiveCallback), state);
-                }
-                else
-                {
-                    // All the data has arrived; put it in response.
-                    if (state.sb.Length > 1)
+                    content = state.sb.ToString();
+                    if (content.IndexOf("<PBIOEOF>") > -1)
                     {
-                        response = state.sb.ToString();
+                        int indexEndTag = content.IndexOf("<PBIOEOF>");
+                        response = content.Remove(indexEndTag);
+
+                        // Signal that all bytes have been received.
+                        receiveDone.Set();
                     }
-                    // Signal that all bytes have been received.
-                    receiveDone.Set();
+                    else
+                    {
+                        // Get the rest of the data.
+                        client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                            new AsyncCallback(ReceiveCallback), state);
+                    }                   
                 }
             }
             catch (Exception e)
